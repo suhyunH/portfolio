a634751923f5757c415cb3d31eef04cc8dced26d
suhyunH <87814333+suhyunH@users.noreply.github.com>
2021-10-26 16:35:38 +0900

    JS: navbar menu click

--- main.js
+++ main.js
@@ -15,3 +15,19 @@ document.addEventListener('scroll',()=>{
     }
 
 });
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event)=>{
+    const target = event.target;
+    const link = target.dataset.link;
+    if(link == null){
+        return;
+    } else{
+        console.log(event.target.dataset.link);
+    }
+
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior: "smooth"});
+
+});
