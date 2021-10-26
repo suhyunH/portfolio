df021848672023c5927f1ddcf92cb1d07fbc1ea4
suhyunH <87814333+suhyunH@users.noreply.github.com>
2021-10-26 19:50:05 +0900

    JS: remove active from nav

--- main.js
+++ main.js
@@ -65,15 +65,23 @@ const workBtnContainer = document.querySelector('.work__categories');
 const projectContainer = document.querySelector('.work__projects');
 const projects = document.querySelectorAll('.project');
 
-workBtnContainer.addEventListener('click', (e)=>{
+workBtnContainer.addEventListener('click', e=>{
     const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
     if(filter == null){
         return;
     }
 
+    // Remove selections from the previous item and select the new one
+    const active = document.querySelector('.categories_btn.selected');
+    active.classList.remove('selected');
+    const target = 
+    e.target.nodeName ==='BUTTON'?e.target: e.target.parentNode;
+    target.classList.add('selected');
+
     projectContainer.classList.add('anim-out');
+   
     setTimeout(()=>{
-        projects.forEach((project)=>{
+        projects.forEach(project=>{
             if(filter ==='*' || filter === project.dataset.type){
                 project.classList.remove('invisible');
             } else{
