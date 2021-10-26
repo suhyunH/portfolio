a3892e75e1ddb205b06c32162e8db9577d192b8c
suhyunH <87814333+suhyunH@users.noreply.github.com>
2021-10-26 20:01:25 +0900

    JS:navbarMenu toggle & project UI tweak

--- main.js
+++ main.js
@@ -16,6 +16,7 @@ document.addEventListener('scroll',()=>{
 
 });
 
+
 // Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event)=>{
@@ -24,8 +25,15 @@ navbarMenu.addEventListener('click', (event)=>{
     if(link == null){
         return;
     } 
-    
+    navbarMenu.classList.remove('open');
     scrollIntoView(link);
+});
+
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', ()=>{
+    navbarMenu.classList.toggle('open');
+
 });
 
 // Handle click on "contact me" button on home
